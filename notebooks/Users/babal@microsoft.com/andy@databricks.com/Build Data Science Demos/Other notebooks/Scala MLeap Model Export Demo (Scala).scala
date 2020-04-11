7cf13e2d671641692e2034afeb3985e9e5fb65fc
Bala <bala_b@hotmail.com>
2020-04-11 07:30:12 -0500

    Update: README.md notebooks

---
+++ notebooks/Users/babal@microsoft.com/andy@databricks.com/Build Data Science Demos/Other notebooks/Scala MLeap Model Export Demo (Scala).scala
@@ -0,0 +1,276 @@
+// Databricks notebook source
+// MAGIC %md #  Model Export with MLeap
+// MAGIC 
+// MAGIC MLeap is a common serialization format and execution engine for machine learning pipelines. It supports Apache Spark, Scikit-learn, and TensorFlow for training pipelines and exporting them to an MLeap Bundle. Serialized pipelines (bundles) can be deserialized back into Apache Spark, Scikit-learn, TensorFlow graphs, or an MLeap pipeline. This notebook only demonstrates how to use MLeap to do the model export with MLlib. For an overview of the package and more examples, check out the [MLeap documentations](http://mleap-docs.combust.ml/).
+
+// COMMAND ----------
+
+// MAGIC %md ## Cluster Setup
+// MAGIC 
+// MAGIC To use MLeap's Spark integration on the cluster, you can create a new library with the source option ``Maven Coordinate`` and Maven artifacts coordinate ``mleap-spark-extension_2.11-0.10.1``. Then [attach the library to a cluster](https://docs.databricks.com/user-guide/libraries.html).
+// MAGIC 
+// MAGIC **Note: **
+// MAGIC * This version of MLeap works with Spark 2.0, 2.1, 2.2, and 2.3. 
+// MAGIC * It is cross-compiled only for Scala 2.11.
+
+// COMMAND ----------
+
+// MAGIC %md ## In this Notebook
+// MAGIC 
+// MAGIC This notebook demonstrates how to use MLeap to export a `DecisionTreeClassifier` from MLlib and how to load the saved PipelineModel to make predictions.
+// MAGIC 
+// MAGIC The basic workflow is as follows:
+// MAGIC * Model export
+// MAGIC   * Fit a PipelineModel using MLlib.
+// MAGIC   * Use MLeap to serialize the PipelineModel to zip File or to directory.
+// MAGIC * Move the PipelineModel files to your deployment project or data store.
+// MAGIC * In your project
+// MAGIC   * Use MLeap to deserialize the saved PipelineModel
+// MAGIC   * Make predictions.
+
+// COMMAND ----------
+
+// MAGIC %md ## Training the Model by MLlib
+
+// COMMAND ----------
+
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation._
+import org.apache.spark.ml.feature._
+import org.apache.spark.ml.tuning._
+
+// COMMAND ----------
+
+val df = spark.read.parquet("/databricks-datasets/news20.binary/data-001/training")
+  .select("text", "topic")
+df.cache()
+display(df)
+
+// COMMAND ----------
+
+df.printSchema()
+
+// COMMAND ----------
+
+// MAGIC %md ### Define ML Pipeline
+
+// COMMAND ----------
+
+val labelIndexer = new StringIndexer()
+  .setInputCol("topic")
+  .setOutputCol("label")
+  .setHandleInvalid("keep")
+
+// COMMAND ----------
+
+val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
+val hashingTF = new HashingTF().setInputCol("words").setOutputCol("features")
+
+// COMMAND ----------
+
+val dt = new DecisionTreeClassifier()
+val pipeline = new Pipeline()
+  .setStages(Array(labelIndexer, tokenizer, hashingTF, dt))
+
+// COMMAND ----------
+
+// MAGIC %md ### Tune ML Pipeline
+
+// COMMAND ----------
+
+val paramGrid = new ParamGridBuilder()
+  .addGrid(hashingTF.numFeatures, Array(1000, 2000))
+  .build()
+val cv = new CrossValidator()
+  .setEstimator(pipeline)
+  .setEvaluator(new MulticlassClassificationEvaluator())
+  .setEstimatorParamMaps(paramGrid)
+
+// COMMAND ----------
+
+val cvModel = cv.fit(df)
+
+// COMMAND ----------
+
+val model = cvModel.bestModel.asInstanceOf[PipelineModel]
+
+// COMMAND ----------
+
+val sparkTransformed = model.transform(df)
+
+display(sparkTransformed)
+
+// COMMAND ----------
+
+// MAGIC %md ## Export Trained Model with MLeap
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC MLeap supports serializing the model to one zip file or to files in one directory. 
+// MAGIC 
+// MAGIC * Serialize to a zip file, make sure the URI begins with ``jar:file`` and ends with a ``.zip``.
+// MAGIC * Serialize to a directory, make sure the URI begins with ``file``.
+
+// COMMAND ----------
+
+// MAGIC %md The supported transformers for MLeap can be found in the  [MLeap documentation](http://mleap-docs.combust.ml/core-concepts/transformers/support.html).
+
+// COMMAND ----------
+
+// MAGIC %sh 
+// MAGIC rm -rf /tmp/mleap_scala_model_export/
+// MAGIC mkdir /tmp/mleap_scala_model_export/
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Serialize to zip file:
+
+// COMMAND ----------
+
+import ml.combust.bundle.BundleFile
+import ml.combust.mleap.spark.SparkSupport._
+import org.apache.spark.ml.bundle.SparkBundleContext
+import resource._
+
+implicit val context = SparkBundleContext().withDataset(sparkTransformed)
+//save our pipeline to a zip file
+//MLeap can save a file to any supported java.nio.FileSystem
+(for(modelFile <- managed(BundleFile("jar:file:/tmp/mleap_scala_model_export/20news_pipeline-json.zip"))) yield {
+  model.writeBundle.save(modelFile)(context)
+}).tried.get
+
+// COMMAND ----------
+
+// MAGIC %md Serialize to directory:
+
+// COMMAND ----------
+
+(for(modelFile <- managed(BundleFile("file:/tmp/mleap_scala_model_export/20news_pipeline"))) yield {
+  model.writeBundle.save(modelFile)(context)
+}).tried.get
+
+// COMMAND ----------
+
+// MAGIC %sh ls /tmp/mleap_scala_model_export/
+
+// COMMAND ----------
+
+// MAGIC %md ## Download Model Files
+// MAGIC 
+// MAGIC In this example we download the model files from the browser. In general, you may want to programmatically move the model to a persistent storage layer.
+
+// COMMAND ----------
+
+// MAGIC %sh
+// MAGIC cp /tmp/mleap_scala_model_export/20news_pipeline-json.zip /dbfs/FileStore/20news_pipeline-json.zip
+
+// COMMAND ----------
+
+// MAGIC %md  Get a link to the downloadable zip via:
+// MAGIC `https://[MY_DATABRICKS_URL]/files/[FILE_NAME].zip`.  E.g., if you access Databricks at `https://mycompany.databricks.com`, then your link would be:
+// MAGIC `https://mycompany.databricks.com/files/20news_pipeline-json.zip`.
+
+// COMMAND ----------
+
+// MAGIC %md ## Import Trained Model with MLeap
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC This section shows how to use MLeap to load a trained model for use in your application. To use an existing ML models and pipelines to make predictions for new data, you can deserialize the model from the file you saved.
+
+// COMMAND ----------
+
+// MAGIC %md ### Import to Spark
+// MAGIC 
+// MAGIC This section shows how to load an MLeap bundle and make predictions on a Spark DataFrame.  This can be useful if you want to use the same persistence format (bundle) for loading into Spark and non-Spark applications.  If your goal is to make predictions only in Spark, then we recommend using [MLlib's native ML persistence](https://spark.apache.org/docs/latest/ml-pipeline.html#ml-persistence-saving-and-loading-pipelines).
+
+// COMMAND ----------
+
+import ml.combust.bundle.BundleFile
+import ml.combust.mleap.spark.SparkSupport._
+import resource._
+
+val zipBundle = (for(bundle <- managed(BundleFile("jar:file:/tmp/mleap_scala_model_export/20news_pipeline-json.zip"))) yield {
+  bundle.loadSparkBundle().get
+}).opt.get
+
+// COMMAND ----------
+
+val loadedModel = zipBundle.root
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Now you can use the loaded model to make predictions with the new data.
+
+// COMMAND ----------
+
+val test_df = spark.read.parquet("/databricks-datasets/news20.binary/data-001/test")
+  .select("text", "topic")
+test_df.cache()
+display(test_df)
+
+// COMMAND ----------
+
+val exampleResults = loadedModel.transform(test_df)
+
+display(exampleResults)
+
+// COMMAND ----------
+
+// MAGIC %md ### Import to MLeap
+// MAGIC 
+// MAGIC This section demonstrates how to import our model into a non-Spark application.  MLeap provides a [`LeapFrame` API](http://mleap-docs.combust.ml/mleap-runtime/create-leap-frame.html) which is essentially a local `DataFrame`.  After importing our model using MLeap, we can make predictions on data stored in a `LeapFrame` without using a `SparkContext` or `SparkSession`.
+
+// COMMAND ----------
+
+import ml.combust.bundle.BundleFile
+import ml.combust.mleap.runtime.MleapSupport._
+import resource._
+
+val zipBundleM = (for(bundle <- managed(BundleFile("jar:file:/tmp/mleap_scala_model_export/20news_pipeline-json.zip"))) yield {
+  bundle.loadMleapBundle().get
+}).opt.get
+
+// COMMAND ----------
+
+val mleapPipeline = zipBundleM.root
+
+// COMMAND ----------
+
+// MAGIC %md Setup a LeapFrame for testing, manually inserting 1 row of data.
+
+// COMMAND ----------
+
+import ml.combust.mleap.runtime.frame.{DefaultLeapFrame, Row}
+import ml.combust.mleap.core.types._
+
+val schema = StructType(StructField("text", ScalarType.String),
+  StructField("topic", ScalarType.String)).get
+val data = Seq(Row("From: marshall@csugrad.cs.vt.edu (Kevin Marshall) Subject: Re: Faith and Dogma Organization: Virginia Tech Computer Science Dept, Blacksburg, VA Lines: 96 NNTP-Posting-Host: csugrad.cs.vt.edu   tgk@cs.toronto.edu (Todd Kelley) writes: >In light of what happened in Waco, I need to get something of my >chest. > >Faith and dogma are dangerous. ", "alt.atheism"))
+val frame = DefaultLeapFrame(schema, data)
+
+// COMMAND ----------
+
+// MAGIC %md Now you can use the loaded model to make predictions.
+
+// COMMAND ----------
+
+val frame2 = mleapPipeline.transform(frame).get
+val data2 = frame2.dataset
+
+// COMMAND ----------
+
+// The prediction is stored in column with index 2:
+frame2.schema.fields.zipWithIndex.foreach { case (field, idx) =>
+  println(s"$idx $field")
+}
+
+// COMMAND ----------
+
+// Get the prediction for Row 0
+data2(0).getDouble(7)
